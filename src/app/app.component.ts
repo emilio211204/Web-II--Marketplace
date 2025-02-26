ba1915945e97fdf30ae7b3d1813def0eab9ddcdb
Mc-Cristian <sangoluisacristian75@gmail.com>
2025-02-26 10:54:48 -0500

    nosotros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HomeComponent } from "./home/home.component";
-import { ServiciosComponent } from './servicios/servicios.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent,ServiciosComponent],
+  imports: [RouterOutlet, NosotrosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
